a57339106148e3cb076de85274c07bfa05da84ca
liona <lionadsouza17@gmail.com>
2017-06-08 12:21:17 +0530

    222

--- src/qwertTest.java
+++ src/qwertTest.java
@@ -2,15 +2,15 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-import newww.qwert;
+
 
 public class qwertTest {
 
 	@Test
 	public void test() {
 		qwert q=new qwert(1000);
-		q.deposit(600);
-		assertEquals(1600,q.getBal());
+		q.deposit(800);
+		assertEquals(1800,q.getBal());
 	}
 
 }
